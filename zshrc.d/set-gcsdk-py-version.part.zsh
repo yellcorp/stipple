4c079d5b789cc7d9229f22b2723d07c749826fa5
Jim Boswell <jimb@yellcorp.org>
2023-08-07 13:25:17 -0700

    gcloud sdk now prefers py3.9 (I think?)

--- zshrc.d/set-gcsdk-py-version.part.zsh
+++ zshrc.d/set-gcsdk-py-version.part.zsh
@@ -1,17 +1,14 @@
-# The gcloud cli "works best" with Python 3.7 for whatever reason(s). If
-# pyenv is in use and a 3.7.x version is installed, this snippet configures
-# gcloud to use that.
-
-# This advice should probably be periodically re-checked:
+# The following link says
+# - Supported versions are 3.5-3.9
+# - The installer for Cloud SDK versions > 352.0.0 offer to install Python 3.7
 # https://cloud.google.com/sdk/docs/install#mac
 #
-# As of version 395, this installer feature was present:
+# However starting in version 433.0.0, the following notice appears:
+# "Note: Support for Python 3.5-3.7 will be deprecated on August 8th, 2023."
+# The CLI also emits this message when running.
 #
-# """
-# For Cloud SDK release version 352.0.0 and above, the main install script
-# offers to install CPython's Python 3.7 on Intel-based Macs.
-# """
-
+# Presumably, the ideal Python version to run is 3.9.
+#
 # This snippet detects pyenv by checking for an env var called PYENV_ROOT,
 # so make sure that is set before running this.
 
@@ -21,14 +18,14 @@ function () {
     local best_exe
     local pydir
 
-    # Find the highest-versioned Python 3.7.x available.
+    # Find the highest-versioned Python 3.9.x available.
     #
     # (N) is a 'Glob Qualifier' - it sets the NULL_GLOB option for the
     # pattern. Otherwise an error will be output if nothing matches
     # see man zshexpn#Glob Qualifiers
     #     man zshoptions#NULL_GLOB
 
-    for pydir in "${PYENV_ROOT}"/versions/3.7.*(N); do
+    for pydir in "${PYENV_ROOT}"/versions/3.9.*(N); do
       # Use zsh :e to isolate the filename 'extension' - i.e. everything
       # after the last dot, which is the patch level number in this context.
       if (( ${pydir:e} > ${best} )) && [[ -x "${pydir}/bin/python3" ]]; then
