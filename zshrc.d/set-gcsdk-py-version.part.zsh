34a8e1036dc57977deb942d72932fc45c2af228f
Jim Boswell <jimb@yellcorp.org>
2024-04-21 23:18:57 -0700

    Simplify gcloud python by just making a venv for it in a well-known dir

--- zshrc.d/set-gcsdk-py-version.part.zsh
+++ zshrc.d/set-gcsdk-py-version.part.zsh
@@ -1,45 +1,71 @@
-# The following link says
-# - Supported versions are 3.5-3.9
-# - The installer for Cloud SDK versions > 352.0.0 offer to install Python 3.7
-# https://cloud.google.com/sdk/docs/install#mac
+# The recommended Python version varies with gcloud version. Find the latest
+# advice here:
 #
-# However starting in version 433.0.0, the following notice appears:
-# "Note: Support for Python 3.5-3.7 will be deprecated on August 8th, 2023."
-# The CLI also emits this message when running.
+#   https://cloud.google.com/sdk/docs/install#mac
 #
-# Presumably, the ideal Python version to run is 3.9.
-#
-# This snippet detects pyenv by checking for an env var called PYENV_ROOT,
-# so make sure that is set before running this.
+# This snippet checks for a dedicated Python venv for use with `gcloud` at a
+# well-known location, then falls back to an older strategy of searching
+# pyenv installed versions. Ideally all computers I use should be migrated
+# to the newer, simpler venv approach, but the old snippet is there while I
+# switch over.
 
-function () {
-  if [[ -z "${CLOUDSDK_PYTHON}" && -n "${PYENV_ROOT}" ]]; then
-    local best=-1
-    local best_exe
-    local pydir
+function {
+  # Only do anything if gcloud's special var is not already set
+  if [[ -z "${CLOUDSDK_PYTHON}" ]]; then
+    local gcloud_venv="${HOME}/gcloud/venv"
+    local gcloud_python="${gcloud_venv}/bin/python3"
 
-    # Find the highest-versioned Python 3.9.x available.
-    #
-    # (N) is a 'Glob Qualifier' - it sets the NULL_GLOB option for the
-    # pattern. Otherwise an error will be output if nothing matches
-    # see man zshexpn#Glob Qualifiers
-    #     man zshoptions#NULL_GLOB
+    if [[ -x "${gcloud_python}" ]]; then
+      # Newer, simpler strat: just make a venv and stick it in a well-known
+      # location. It requires a slightly ugly dir arrangement, installing
+      # gcloud into
+      #
+      #   ~/gcloud/google-cloud-sdk
+      #
+      # but the simplification is worth it.
+      #
+      # Make the necessary venv with
+      # ${PYTHON} -v venv ~/gcloud/venv
+      #
+      # If some subcomponent of gcloud wants you to install some Python
+      # package:
+      #
+      # ~/gcloud/venv/bin/pip3 install [packages...]
 
-    for pydir in "${PYENV_ROOT}"/versions/3.9.*(N); do
-      # Use zsh :e to isolate the filename 'extension' - i.e. everything
-      # after the last dot, which is the patch level number in this context.
-      if (( ${pydir:e} > ${best} )) && [[ -x "${pydir}/bin/python3" ]]; then
-        best=${pydir:e}
-        best_exe="${pydir}/bin/python3"
+      export CLOUDSDK_PYTHON="${gcloud_python}"
+      # TODO: do we need CLOUDSDK_PYTHON_SITEPACKAGES ?
+
+    elif [[ -n "${PYENV_ROOT}" ]]; then
+      # Older overwrought way which I thought was clever at the time. Grovel
+      # pyenv, if available, for an ideal Python version.
+
+      local best=-1
+      local best_exe
+      local pydir
+
+      # Find the highest-versioned Python 3.9.x available.
+      #
+      # (N) is a 'Glob Qualifier' - it sets the NULL_GLOB option for the
+      # pattern. Otherwise an error will be output if nothing matches
+      # see man zshexpn#Glob Qualifiers
+      #     man zshoptions#NULL_GLOB
+
+      for pydir in "${PYENV_ROOT}"/versions/3.9.*(N); do
+        # Use zsh :e to isolate the filename 'extension' - i.e. everything
+        # after the last dot, which is the patch level number in this context.
+        if (( ${pydir:e} > ${best} )) && [[ -x "${pydir}/bin/python3" ]]; then
+          best=${pydir:e}
+          best_exe="${pydir}/bin/python3"
+        fi
+      done
+
+      if [[ -n "${best_exe}" ]]; then
+        export CLOUDSDK_PYTHON="${best_exe}"
+
+        # Make sure `gcloud compute ssh` can see numpy if you follow the nag
+        # to install it
+        export CLOUDSDK_PYTHON_SITEPACKAGES=1
       fi
-    done
-
-    if [[ -n "${best_exe}" ]]; then
-      export CLOUDSDK_PYTHON="${best_exe}"
-
-      # Make sure `gcloud compute ssh` can see numpy if you follow the nag
-      # to install it
-      export CLOUDSDK_PYTHON_SITEPACKAGES=1
     fi
   fi
 }
